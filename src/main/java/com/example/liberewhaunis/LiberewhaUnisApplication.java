312cdf92b5fe8dc1a362cb413c816401555aa9cf
gyesswhat <h2weirdo@icloud.com>
2024-05-02 23:03:44 +0900

    chore: 기본 파일 세팅

---
+++ src/main/java/com/example/liberewhaunis/LiberewhaUnisApplication.java
@@ -0,0 +1,13 @@
+package com.example.liberewhaunis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberewhaUnisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberewhaUnisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liberewhaunis/LiberewhaUnisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liberewhaunis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberewhaUnisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

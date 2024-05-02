259e9688cb8c58422af51dd9072da11f99e2dabe
gyesswhat <h2weirdo@icloud.com>
2024-05-03 00:42:30 +0900

    chore: 빌드 확인용 test controller

---
+++ src/main/java/com/example/liberewhaunis/TestController.java
@@ -0,0 +1,12 @@
+package com.example.liberewhaunis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String helloworld() {
+        return "Hello, world!";
+    }
+}
